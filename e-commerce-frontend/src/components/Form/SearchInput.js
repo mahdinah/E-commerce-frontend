2dbd88babb0a903af427f7d0b1531937f9c17658
mahdi halloum <mahdinah98@gmail.com>
2023-04-11 12:24:36 +0300

    step 1 navbar

--- e-commerce-frontend/src/components/Form/SearchInput.js
+++ e-commerce-frontend/src/components/Form/SearchInput.js
@@ -20,11 +20,13 @@ const SearchInput = () => {
   };
   return (
     <div>
-      <form
+      <form 
         className="d-flex search-form"
         role="search"
         onSubmit={handleSubmit}
+        display="none"
       >
+        <div className="together">
         <input
           className="form-control me-2"
           type="search"
@@ -33,9 +35,19 @@ const SearchInput = () => {
           value={values.keyword}
           onChange={(e) => setValues({ ...values, keyword: e.target.value })}
         />
-        <button className="btn btn-outline-success" type="submit">
-          Search
+        <button className="btn btn-outline-success" type="submit" >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>{" "}
         </button>
+        </div>
       </form>
     </div>
   );
--- e-commerce-frontend/src/components/Layout/Footer.js
+++ e-commerce-frontend/src/components/Layout/Footer.js
@@ -1,8 +1,110 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="ct-container" data-columns-divider="md">
+        <div data-column="widget-area-1">
+          <div
+            className="ct-widget widget_block widget_media_image"
+            id="block-7"
+          >
+
+          </div>
+          <div className="ct-widget widget_block" id="block-9">
+            <div className="wp-block-group is-layout-flow">
+              <p>
+                Phone: (+63) 555 1212
+                <br />
+                Fax: (+63) 555 0100
+              </p>
+              <p>Email: info@mail.com</p>
+              <p>
+                Address: 304 North Cardinal St.
+                <br />
+                Dorchester Center, MA 02124
+              </p>
+            </div>
+          </div>
+        </div>
+        <div data-column="widget-area-2">
+          <div className="ct-widget widget_block" id="block-12">
+            <div className="wp-block-group is-layout-flow">
+              <h4 className="has-medium-font-size wp-block-heading">
+                Main Menu
+              </h4>
+              <p>
+                <a href="#">Home</a>
+                <br />
+                <a href="#">Products</a>
+                <br />
+                <a href="#">About us</a>
+                <br />
+                <a href="#">Blog</a>
+                <br />
+                <a href="#">Contact</a>
+                <br />
+                <a href="#">Hot Offers</a>
+                <br />
+                <a href="#">Terms &amp; Conditions</a>
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+        <div data-column="widget-area-3">
+          <div className="ct-widget widget_block" id="block-13">
+            <div className="wp-block-group is-layout-flow">
+              <h4 className="has-medium-font-size wp-block-heading">Account</h4>
+              <p>
+                <a href="#">My Account</a>
+                <br />
+                <a href="#">My Wishlist</a>
+                <br />
+                <a href="#">My Cart</a>
+                <br />
+                <a href="#">Sign In</a>
+                <br />
+                <a href="https://demo.creativethemes.com/blocksy/floreo/?page_id=13#">
+                  Specials
+                </a>
+                <br />
+                <a href="#">Check Out</a>
+                <br />
+                <a href="https://demo.creativethemes.com/blocksy/floreo/wp-admin/widgets.php#">
+                  Support Center
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div data-column="widget-area-4">
+          <div className="ct-widget widget_block" id="block-14">
+            <div className="wp-block-group is-layout-flow">
+              <h4 className="has-medium-font-size wp-block-heading">
+                Shipping and Payments
+              </h4>
+              <p>
+                <a href="#">Best Sellers</a>
+                <br />
+                <a href="#">Manufacturers</a>
+                <br />
+                <a href="#">Suppliers</a>
+              </p>
+            </div>
+          </div>
+          <div className="ct-widget widget_block" id="block-16">
+            <div className="wp-block-group is-layout-flow">
+              <h4 className="has-medium-font-size wp-block-heading">
+                Payment Methods
+              </h4>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
       <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
