80bda120c6884787ebd8b4a4826898878b0bce65
mahdi halloum <mahdinah98@gmail.com>
2023-05-03 00:30:11 +0300

    finishing user profile

--- e-commerce-frontend/src/components/Form/CategoryForm.js
+++ e-commerce-frontend/src/components/Form/CategoryForm.js
@@ -1,20 +1,25 @@
 import React from "react";
+import "../../styles/Admin-User-panel.css"
 
 const CategoryForm = ({ handleSubmit, value, setValue }) => {
+  
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
+      <form className="catcreate" onSubmit={handleSubmit}>
+        <div className="catcreate-input">
           <input
+        
             type="text"
             className="form-control"
             placeholder="Enter new category"
             value={value}
             onChange={(e) => setValue(e.target.value)}
+           
+            required
           />
         </div>
 
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="submit-cat" >
           Submit
         </button>
       </form>
--- e-commerce-frontend/src/pages/user/Dashboard.js
+++ e-commerce-frontend/src/pages/user/Dashboard.js
@@ -2,20 +2,35 @@ import React from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Layout/UserMenu";
 import { useAuth } from "../../context/auth";
+import { Link } from "react-router-dom";
+import "../../styles/Admin-User-panel.css";
+import authimh from "../../Images/auth.png";
+import coverauth from "../../Images/coveauth.jpeg";
+
 const Dashboard = () => {
   const [auth] = useAuth();
   return (
     <Layout title={"Dashboard - Ecommerce App"}>
       <div className="container-flui m-3 p-3 dashboard">
-        <div className="row">
+        <div className="admin-dash">
           <div className="col-md-3">
             <UserMenu />
           </div>
-          <div className="col-md-9">
-            <div className="card w-75 p-3">
-              <h3>{auth?.user?.name}</h3>
-              <h3>{auth?.user?.email}</h3>
-              <h3>{auth?.user?.address}</h3>
+
+          <div className="col-admin-pan">
+            <div className="admin-panel user-panel">
+              <div className="authimg">
+              <img class="coverauthim" src={coverauth} alt="coverauthimage"/>
+                <Link  to="/dashboard/admin/">
+                
+                <img class="authim" src={authimh} alt="authimage"/>
+             </Link>
+              </div>
+              <Link  to="/dashboard/admin/">
+              <div className="adminauthname">{auth?.user?.name}</div></Link>
+              <div className="adminauthemail">{auth?.user?.email}</div>
+              <div className="adminauthanswer">"My favorite sport is <br/>{auth?.user?.answer}" </div>
+              <div className="adminauthphone">Phone : {auth?.user?.phone}</div>
             </div>
           </div>
         </div>
